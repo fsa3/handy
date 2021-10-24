4bf10b1616ec3e0e6fa5744ef06db087d5fa6118
Fannar Steinn <fannarsteinn@hotmail.com>
2021-10-24 21:34:13 +0200

    first commit

---
+++ src/test/java/is/hi/handy/HandyApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.handy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
