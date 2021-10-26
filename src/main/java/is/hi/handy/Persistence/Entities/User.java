23897cfe08a3ca39e62141b3fddb503017a1bd3c
kak25 <kak25@hi.is>
2021-10-26 21:50:09 +0000

    User Class fyrsta útgáfa. Þarf að skoða betur listan Adsvertisements.

--- src/main/java/is/hi/handy/Persistence/Entities/User.java
+++ src/main/java/is/hi/handy/Persistence/Entities/User.java
@@ -1,6 +1,8 @@
 package is.hi.handy.Persistence.Entities;
 
 import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "users")
@@ -8,4 +10,38 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
+    private String email;
+    private String Info;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Ad> adsvertisements = new ArrayList<>();
+    //todo messages
+
+    public User(){
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Ad> getAds() {
+        return adsvertisements;
+    }
+
+    public void setAds(List<Ad> ads) {
+        this.adsvertisements = ads;
+    }
 }
