cf3e1fcdd2aa5bc5642105711258361fee81da14
Fannar Steinn <fannarsteinn@hotmail.com>
2021-10-27 18:20:02 +0200

    signup request mapping

--- src/main/java/is/hi/handy/Controllers/UserController.java
+++ src/main/java/is/hi/handy/Controllers/UserController.java
@@ -3,10 +3,11 @@ package is.hi.handy.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
-    @RequestMapping("/signup")
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
     public String signUpPage(Model model) {
         return "signup";
     }
