0fbb3d89c8c9e014fde8eb0a90838465da3b21de
kak25 <kak25@hi.is>
2021-10-30 16:35:18 +0000

    AdController , ads.html smá breytingar á hlekknum Create new Ad

--- src/main/java/is/hi/handy/Controllers/AdController.java
+++ src/main/java/is/hi/handy/Controllers/AdController.java
@@ -1,8 +1,14 @@
 package is.hi.handy.Controllers;
 
+import is.hi.handy.Services.AdService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AdController {
+private AdService adService;
+
+public AdController(AdService adService){this.adService = adService;}
+
+
 
 }
