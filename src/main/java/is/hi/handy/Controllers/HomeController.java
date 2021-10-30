65f68883c3dac577be294ad9c880d8ab8ccb8b04
fredburger11 <32545517+fredburger11@users.noreply.github.com>
2021-10-30 14:16:20 +0000

    aðeins breyttur home controller

--- src/main/java/is/hi/handy/Controllers/HomeController.java
+++ src/main/java/is/hi/handy/Controllers/HomeController.java
@@ -3,6 +3,7 @@ package is.hi.handy.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -11,4 +12,10 @@ public class HomeController {
     public String homePage(Model model) {
         return "home";
     }
+
+    @RequestMapping(value = "/information", method = RequestMethod.GET)
+    public String infoPage(){
+        return "information";
+    }
+
 }
