74714db8fec840a7abc1f3cf1e053254d82a2f43
Fannar Steinn <fannarsteinn@hotmail.com>
2021-11-01 21:42:40 +0100

    ad tengist við þann user sem býr það til

--- src/main/java/is/hi/handy/Controllers/AdController.java
+++ src/main/java/is/hi/handy/Controllers/AdController.java
@@ -1,6 +1,7 @@
 package is.hi.handy.Controllers;
 
 import is.hi.handy.Persistence.Entities.Ad;
+import is.hi.handy.Persistence.Entities.User;
 import is.hi.handy.Services.AdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /* Þetta er í vinnslu*/
@@ -31,18 +33,23 @@ public AdController(AdService adService){this.adService = adService;}
     }
 
     @RequestMapping(value = "/createad", method = RequestMethod.GET)
-    public String adFormGet(Model model){
-        return "createAd";
+    public String adFormGet(Model model, HttpSession session){
+        User loggedInUser = (User) session.getAttribute("LoggedInUser");
+        if(loggedInUser != null) {
+            return "createAd";
         }
+        return "login";
+    }
 
     @RequestMapping(value = "/createad", method = RequestMethod.POST)
-       public String saveAd(Ad ad, BindingResult result, Model model){
-
-            System.out.println(ad);
-             if(result.hasErrors()){
-                  return "createad";
-             }
-             adService.save(ad);
-             return "redirect:/ads";
-       }
+    public String saveAd(Ad ad, BindingResult result, Model model, HttpSession session){
+        System.out.println(ad);
+        if(result.hasErrors()){
+            return "createad";
+        }
+        User loggedInUser = (User) session.getAttribute("LoggedInUser");
+        ad.setUser(loggedInUser);
+        adService.save(ad);
+        return "redirect:/ads";
+    }
 }
