43910561f2e4e30dfe9f2962151167336243eee8
fredburger11 <32545517+fredburger11@users.noreply.github.com>
2021-11-02 01:40:30 +0000

    detail info fyrir hvert ad og misheppnað delete

--- src/main/java/is/hi/handy/Controllers/AdController.java
+++ src/main/java/is/hi/handy/Controllers/AdController.java
@@ -1,17 +1,20 @@
 package is.hi.handy.Controllers;
 
 import is.hi.handy.Persistence.Entities.Ad;
+import is.hi.handy.Persistence.Entities.HandyUser;
 import is.hi.handy.Persistence.Entities.User;
 import is.hi.handy.Services.AdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 import javax.servlet.http.HttpSession;
+import java.nio.file.FileStore;
 import java.util.List;
 
 /* Þetta er í vinnslu*/
@@ -26,11 +29,21 @@ public AdController(AdService adService){this.adService = adService;}
     @RequestMapping("/ads")
      public String adForm(Model model) {
 
-        List<Ad> advertisements = adService.findAll();
+        List<Ad> advertisements = adService.findAllOrderByTimePostedDesc();
         model.addAttribute("ads", advertisements);
 
         return "ads";
     }
+
+    @RequestMapping(value = "/ads/{id}", method = RequestMethod.GET)
+    public String adInfoGet(@PathVariable("id") long id, Model model){
+
+        Ad detailedAd = adService.findOne(id);
+        model.addAttribute("ad", detailedAd);
+        return "adInfo";
+    }
+
+
 
     @RequestMapping(value = "/createad", method = RequestMethod.GET)
     public String adFormGet(Model model, HttpSession session){
@@ -50,6 +63,18 @@ public AdController(AdService adService){this.adService = adService;}
         User loggedInUser = (User) session.getAttribute("LoggedInUser");
         ad.setUser(loggedInUser);
         adService.save(ad);
+        return "redirect:/ads";
+    }
+
+    @RequestMapping(value = "/ads/delete/{id}", method = RequestMethod.GET)
+    public String deleteAd(@PathVariable("id") long id, HttpSession session, Model model) {
+        Ad adToDelete = adService.findOne(id);
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        System.out.println(sessionUser);
+        System.out.println(adToDelete.getUser());
+        if(sessionUser == adToDelete.getUser()){ //todo er ekki að virka eins skildi
+            adService.delete(adToDelete);
+        }
         return "redirect:/ads";
     }
 }
