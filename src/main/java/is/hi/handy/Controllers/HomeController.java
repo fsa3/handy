0bdf4d8d9f8155fb2dbeedb1083b07229e4a1f1f
Fannar Steinn <fannarsteinn@hotmail.com>
2021-11-17 18:48:28 +0100

    grunn útlt sett á home page og header og footer

--- src/main/java/is/hi/handy/Controllers/HomeController.java
+++ src/main/java/is/hi/handy/Controllers/HomeController.java
@@ -1,15 +1,20 @@
 package is.hi.handy.Controllers;
 
+import is.hi.handy.Persistence.Entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String homePage(Model model) {
+    public String homePage(Model model, HttpSession session) {
+        User loggedInUser = (User) session.getAttribute("LoggedInUser");
+        model.addAttribute("LoggedInUser", loggedInUser);
         return "home";
     }
 
