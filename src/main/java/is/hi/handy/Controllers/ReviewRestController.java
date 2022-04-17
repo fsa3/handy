45fda28716323f3dfafd777e0001eea00a11599b
kak25 <kak25@hi.is>
2022-04-17 12:45:33 +0000

    ReviewRestController settur upp.

--- src/main/java/is/hi/handy/Controllers/ReviewRestController.java
+++ src/main/java/is/hi/handy/Controllers/ReviewRestController.java
@@ -1,15 +1,11 @@
 package is.hi.handy.Controllers;
 
 import is.hi.handy.Persistence.Entities.Review;
-import is.hi.handy.Persistence.Entities.User;
 import is.hi.handy.Services.ReviewService;
 import is.hi.handy.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,12 +20,9 @@ public class ReviewRestController {
         this.userService = userService;
     }
 
-    @RequestMapping(value ="/api/reviews", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<Review> getReviews(@RequestParam(value = "user", required = false) User user)  {
-        List<Review> Reviews;
-        if(user != null){ Reviews = getReviews(user);}
-        else
-            Reviews = null;
+    @RequestMapping(value ="/api/reviews/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Review> getReviews(@PathVariable long userId)  {
+        List<Review> Reviews = getReviews(userId);
 
         return Reviews;
     }
