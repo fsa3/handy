1bfde3a50cb31d5b203fb7a98e1307af29439871
Fannar Steinn Adalsteinsson <fsa3@hi.is>
2022-04-20 02:41:05 +0000

    allt komið í ReviewRestController

--- src/main/java/is/hi/handy/Controllers/ReviewRestController.java
+++ src/main/java/is/hi/handy/Controllers/ReviewRestController.java
@@ -1,7 +1,9 @@
 package is.hi.handy.Controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import is.hi.handy.Persistence.Entities.HandyUser;
 import is.hi.handy.Persistence.Entities.Review;
+import is.hi.handy.Persistence.Entities.User;
 import is.hi.handy.Services.ReviewService;
 import is.hi.handy.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +36,38 @@ public class ReviewRestController {
         return reviews;
     }
 
+    @RequestMapping(value ="/api/reviews-written/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Review> getReviewsWrittenByUser(@PathVariable long userId)  {
+        User user = userService.findUser(userId);
+        List<Review> reviews = reviewService.findByAuthor(user);
 
+        for (Review r : reviews) {
+            r.getHandyman().setAds(null);
+            r.getAuthor().setAds(null);
+        }
+
+        return reviews;
+    }
+
+    @RequestMapping(value ="/api/reviews", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Review postReview(@RequestBody ObjectNode json)  {
+        Review review = new Review();
+
+        User author = userService.findUser(json.get("author").asLong());
+        HandyUser handyman = userService.findOneHandyUser(json.get("handyman").asLong());
+        String text = json.get("text").asText();
+        int rating = json.get("rating").asInt();
+
+        review.setAuthor(author);
+        review.setHandyman(handyman);
+        review.setText(text);
+        review.setRating(rating);
+
+        Review savedReview = reviewService.save(review);
+
+        savedReview.getAuthor().setAds(null);
+        savedReview.getHandyman().setAds(null);
+
+        return savedReview;
+    }
 }
